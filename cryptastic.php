e46c4f937992544bd7f862bd29603de00ce2f778
unknown <inventuminteractive@gmail.com>
2014-02-27 00:26:48 -0500

    Cleaned up formatting

--- cryptastic.php
+++ cryptastic.php
@@ -29,14 +29,15 @@
 
 class cryptastic
 {
-	/** Encryption Procedure
+	/**
+	 * Encryption Procedure
 	 *
-	 *	@param   mixed    msg      message/data
-	 *	@param   string   k        encryption key
-	 *	@param   boolean  base64   base64 encode result
+	 * @param   mixed    msg      message/data
+	 * @param   string   k        encryption key
+	 * @param   boolean  base64   base64 encode result
 	 *
-	 *	@return  string   iv+ciphertext+mac or
-	 *           boolean  false on error
+	 * @return  string   iv+ciphertext+mac or
+	 *          boolean  false on error
 	*/
 	public function encrypt( $msg, $k, $base64 = false )
 	{
@@ -44,76 +45,78 @@ class cryptastic
 		if ( ! $td = mcrypt_module_open('rijndael-256', '', 'ctr', '') )
 			return false;
 
-		$msg = serialize($msg);							# serialize
-		$iv  = mcrypt_create_iv(32, MCRYPT_RAND);		# create iv
+		$msg = serialize($msg);                         # serialize
+		$iv  = mcrypt_create_iv(32, MCRYPT_RAND);       # create iv
 
-		if ( mcrypt_generic_init($td, $k, $iv) !== 0 )	# initialize buffers
+		if ( mcrypt_generic_init($td, $k, $iv) !== 0 )  # initialize buffers
 			return false;
 
-		$msg  = mcrypt_generic($td, $msg);				# encrypt
-		$msg  = $iv . $msg;								# prepend iv
-		$mac  = $this->pbkdf2($msg, $k, 1000, 32);		# create mac
-		$msg .= $mac;									# append mac
+		$msg  = mcrypt_generic($td, $msg);              # encrypt
+		$msg  = $iv . $msg;                             # prepend iv
+		$mac  = $this->pbkdf2($msg, $k, 1000, 32);      # create mac
+		$msg .= $mac;                                   # append mac
 
-		mcrypt_generic_deinit($td);						# clear buffers
-		mcrypt_module_close($td);						# close cipher module
+		mcrypt_generic_deinit($td);                     # clear buffers
+		mcrypt_module_close($td);                       # close cipher module
 
-		if ( $base64 ) $msg = base64_encode($msg);		# base64 encode?
+		if ( $base64 ) $msg = base64_encode($msg);      # base64 encode?
 
-		return $msg;									# return iv+ciphertext+mac
+		return $msg;                                    # return iv+ciphertext+mac
 	}
-	/** Decryption Procedure
+	/**
+	 * Decryption Procedure
 	 *
-	 *	@param   string   msg      output from encrypt()
-	 *	@param   string   k        encryption key
-	 *	@param   boolean  base64   base64 decode msg
+	 * @param   string   msg      output from encrypt()
+	 * @param   string   k        encryption key
+	 * @param   boolean  base64   base64 decode msg
 	 *
-	 *	@return  string   original message/data or
-	 *           boolean  false on error
+	 * @return  string   original message/data or
+	 *          boolean  false on error
 	*/
 	public function decrypt( $msg, $k, $base64 = false )
 	{
-		if ( $base64 ) $msg = base64_decode($msg);			# base64 decode?
+		if ( $base64 ) $msg = base64_decode($msg);          # base64 decode?
 
 		# open cipher module (do not change cipher/mode)
 		if ( ! $td = mcrypt_module_open('rijndael-256', '', 'ctr', '') )
 			return false;
 
-		$iv  = substr($msg, 0, 32);							# extract iv
-		$mo  = strlen($msg) - 32;							# mac offset
-		$em  = substr($msg, $mo);							# extract mac
-		$msg = substr($msg, 32, strlen($msg)-64);			# extract ciphertext
-		$mac = $this->pbkdf2($iv . $msg, $k, 1000, 32);		# create mac
+		$iv  = substr($msg, 0, 32);                         # extract iv
+		$mo  = strlen($msg) - 32;                           # mac offset
+		$em  = substr($msg, $mo);                           # extract mac
+		$msg = substr($msg, 32, strlen($msg)-64);           # extract ciphertext
+		$mac = $this->pbkdf2($iv . $msg, $k, 1000, 32);     # create mac
 
-		if ( $em !== $mac )									# authenticate mac
+		if ( $em !== $mac )                                 # authenticate mac
 			return false;
 
-		if ( mcrypt_generic_init($td, $k, $iv) !== 0 )		# initialize buffers
+		if ( mcrypt_generic_init($td, $k, $iv) !== 0 )      # initialize buffers
 			return false;
 
-		$msg = mdecrypt_generic($td, $msg);					# decrypt
-		$msg = unserialize($msg);							# unserialize
+		$msg = mdecrypt_generic($td, $msg);                 # decrypt
+		$msg = unserialize($msg);                           # unserialize
 
-		mcrypt_generic_deinit($td);							# clear buffers
-		mcrypt_module_close($td);							# close cipher module
+		mcrypt_generic_deinit($td);                         # clear buffers
+		mcrypt_module_close($td);                           # close cipher module
 
-		return $msg;										# return original msg
+		return $msg;                                        # return original msg
 	}
-	/** PBKDF2 Implementation (as described in RFC 2898);
+	/**
+	 * PBKDF2 Implementation (as described in RFC 2898);
 	 *
-	 *	@param   string  p   password
-	 *	@param   string  s   salt
-	 *	@param   int     c   iteration count (use 1000 or higher)
-	 *	@param   int     kl  derived key length
-	 *	@param   string  a   hash algorithm
+	 * @param   string  p   password
+	 * @param   string  s   salt
+	 * @param   int     c   iteration count (use 1000 or higher)
+	 * @param   int     kl  derived key length
+	 * @param   string  a   hash algorithm
 	 *
-	 *	@return  string  derived key
+	 * @return  string  derived key
 	*/
 	public function pbkdf2( $p, $s, $c, $kl, $a = 'sha256' )
 	{
-		$hl = strlen(hash($a, null, true));	# Hash length
-		$kb = ceil($kl / $hl);				# Key blocks to compute
-		$dk = '';							# Derived key
+		$hl = strlen(hash($a, null, true)); # Hash length
+		$kb = ceil($kl / $hl);              # Key blocks to compute
+		$dk = '';                           # Derived key
 
 		# Create key
 		for ( $block = 1; $block <= $kb; $block ++ )
